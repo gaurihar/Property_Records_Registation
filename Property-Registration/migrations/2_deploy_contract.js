337150287ef78ccbd12e73678b56c99825060c06
gaurihar <gaurihardarade@gmail.com>
2020-07-18 15:21:01 +0530

    read it

--- Property-Registration/migrations/2_deploy_contract.js
+++
@@ -1,6 +0,0 @@
-var landRegistration = artifacts.require("./landRegistration.sol");
-module.exports = function(deployer) {
-  // Use deployer to state migration tasks.
-deployer.deploy(landRegistration);
-
-};
